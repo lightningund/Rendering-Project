b2a511d8b4ca21f5f558e216cd9f27cfa69f8057
lightningund <lightningundies0826@gmail.com>
2026-01-20 15:25:46 -0600

    converted display

---
+++ display.cpp
@@ -0,0 +1,18 @@
+#include "display.hpp"
+
+color_t pixels[DISP_BUF_SIZE * DISP_BUF_SIZE];
+
+// Initialize the display, must be called first
+void display_init() {}
+// Clear the screen (does not affect the framebuffer)
+void display_clear() {}
+// Clear a section of the screen (does not affect the framebuffer)
+void display_clear_section(ScreenRect dim) {}
+// Update the entire screen to reflect the framebuffer
+void display_update_screen() {}
+// Update only a section of the screen
+void display_update_section(ScreenRect dim) {}
+
+void display_set_pixel(uint8_t x, uint8_t y, color_t color) {
+	pixels[y * DISP_BUF_SIZE + x + 0x10] = color;
+}
